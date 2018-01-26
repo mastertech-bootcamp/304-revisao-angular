33ac606a3dfec2a60ed65ae913cf1ed094969a02
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-26 14:52:15 -0200

    Finaliza o jogo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  peixes= [];
+  contador= 0; 
+  constructor () {
+   for (let i = 0; i < 10; i++) {
+     this.inserirPeixe();
+
+   }
+    console.log(this.peixes);
+  }
+  
+  inserirPeixe(){
+    let peixe = {
+      altura: Math.random()*500 + 100 + 'px',
+      largura: Math.random()*500 + 'px', 
+      pescado:false
+    };
+
+    this.peixes.push(peixe);
+
+  }
+
+  tirarPeixe (umPeixe) {
+    umPeixe.pescado = true;
+    this.contador = this.contador +1;
+    console.log(this.contador);
+
+  }
+  
 }
--- src/app/cabecalho/cabecalho.component.ts
+++ src/app/cabecalho/cabecalho.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cabecalho',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cabecalho.component.css']
 })
 export class CabecalhoComponent implements OnInit {
+  @Input() pontos;
 
   constructor() { }
 
